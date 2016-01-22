1b9b0362c159b2d307f97d7e786dfc449b1ff530
Robert Campion <2012rcampion@gmail.com>
2016-01-21 21:02:50 -0600

    Initial commit

---
+++ client/app/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+angular.module('dreddApp', [
+  'dreddApp.constants',
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'btford.socket-io',
+  'ui.router',
+  'ui.bootstrap'
+])
+  .config(function($urlRouterProvider, $locationProvider) {
+    $urlRouterProvider
+      .otherwise('/');
+
+    $locationProvider.html5Mode(true);
+  });
