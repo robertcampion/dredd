24bcc0546c831225cc66f31c52003052b1ee6871
Robert Campion <2012rcampion@gmail.com>
2016-09-18 09:54:02 -0400

    remove 404 page, replace with sendStatus(404)

--- server/routes.js
+++ server/routes.js
@@ -4,7 +4,6 @@
 
 'use strict';
 
-import errors from './components/errors';
 import path from 'path';
 
 export default function(app) {
@@ -15,7 +14,7 @@ export default function(app) {
   app.use('/timesync', require('./timesync'));
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
-   .get(errors[404]);
+   .get((req, res) => { res.sendStatus(404) });
 
   // All other routes should redirect to the index.html
   app.route('/*')
