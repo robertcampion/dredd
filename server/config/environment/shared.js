6e31b32dfd9a52cf50823de4d6e17928eefc2023
Robert Campion <2012rcampion@gmail.com>
2017-03-11 16:52:37 -0600

    last-minute contest hotfixes

--- server/config/environment/shared.js
+++ server/config/environment/shared.js
@@ -12,17 +12,18 @@
 
 var prototypes = [
   { name: 'Traversed tunnel', kind: 'points',     value:   5, count: 1 },
-  { name: 'Traversed tunnel (before 45 sec)', kind: 'points',     value:   10, count: 1 },
+  { name: 'Tunnel before 45s', kind: 'points',     value:   10, count: 1 },
   { name: 'Inserted key',          kind: 'points',     value:   10, count: 1 },
-  { name: 'Team/neutral ball',         kind: 'points',     value:  20, count: 6 },
+  { name: 'Regular ball',         kind: 'points',     value:  20, count: 6 },
   { name: 'Bonus ball', kind: 'points',     value:  30, count: 2 },
-  //{ name: 'Golf ball in team bin',   kind: 'points',     value:  10, count: 16 },
+  { name: 'Late ball',   kind: 'points',     value:  10 },
   { name: 'Personal foul',           kind: 'extra',      value: -10, count: 5 },
   { name: 'Technical foul',          kind: 'extra',      value: -50           },
   { name: 'Assist stuck robot',      kind: 'extra',      value:  +5           },
   { name: 'Airborne robot',          kind: 'multiplier', value:   2 },
   { name: 'Autonomous robot',        kind: 'multiplier', value:   4 },
-  { name: 'Reset multiplier',        kind: 'multiplier', value:   1 }
+  { name: 'Reset multiplier',        kind: 'multiplier', value:   1 },
+  { name: 'Disabled',        kind: 'points', value:   0 }
 ];
 
 // assign ID by index
